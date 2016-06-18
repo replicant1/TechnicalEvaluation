c1ec4189f2c834bedf727913890a2007d31b67d2
rodbailey <rodney.p.bailey@gmail.com>
2016-06-18 21:31:53 +1000

    Incorporated CardView from v7 supoort libraries.

--- app/src/main/java/tech/bailey/rod/PlaceholderFragment.java
+++ app/src/main/java/tech/bailey/rod/PlaceholderFragment.java
@@ -35,9 +35,9 @@ public class PlaceholderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_placeholder, container, false);
-        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
+        View rootView = inflater.inflate(R.layout.fragment_scenario_1, container, false);
+//        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+//        textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
         return rootView;
     }
 }
