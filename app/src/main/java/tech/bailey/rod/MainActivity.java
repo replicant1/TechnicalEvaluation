74d3f0c9c328b6456a27691c512f901092f181a9
rodbailey <rodney.p.bailey@gmail.com>
2016-06-19 19:50:19 +1000

    Commented out CoordinatorLayout usage, which seems to be buggy and lets tab body slip behind the navigation bar, even when you have fitSystemWindows set to true.

--- app/src/main/java/tech/bailey/rod/MainActivity.java
+++ app/src/main/java/tech/bailey/rod/MainActivity.java
@@ -29,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mScenarioPagerAdapter = new ScenarioPagerAdapter(getSupportFragmentManager());
